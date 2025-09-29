7b4a991a5ceabbc72c69d454b40ec41033f0e853
Grzegorz-Bartos <grzes.dzes@gmail.com>
2025-09-29 19:58:08 +0200

    chore(urls): add root route using TemplateView to render base.html

--- src/demoapp/urls.py
+++ src/demoapp/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-
-from textprocessor.views import UploadView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="base.html"), name="home"),
     path("text/", include("textprocessor.urls")),
     path("pesel/", include("peselvalidator.urls")),
-    path("", UploadView.as_view(), name="home"),
 ]
